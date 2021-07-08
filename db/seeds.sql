db3e85a88b402e83bc7bee3f7e15f332c8b8bc67
michaelab24 <michaelbarnes459@yahoo.com>
2021-07-08 16:49:20 -0500

    Tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,5 +4,25 @@ use employees;
 INSERT INTO department
     (name)
 VALUES
-(""),
-("")
+('HR'),
+('Shipping'),
+('Receiving'),
+('Sales'),
+('Engineering');
+
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+('Manager', 125000, 1),
+('Assistang Manager', 100000, 2),
+('Employee', 90000, 3),
+('Intern', 40000, 4);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+('Simon', 'Rennocks', 1, 0),
+('Mark', 'Messier', 2, 1),
+('Tie', 'Domi', 3, 2),
+('Al', 'Robinson', 4, 3);
